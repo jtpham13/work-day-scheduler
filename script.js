b8baa41cb586ae8fdcb61f899e9a597e5e7b3e89
Joey Pham <joeypham@joeys-mbp.mynetworksettings.com>
2023-04-20 23:54:10 -0400

    added current date

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// added a button for each hour
+// added button for each hour 
 var saveButtons = document.querySelectorAll(".btn");
 console.log(saveButtons);
 saveButtons.forEach(function (individualButton) {
   individualButton.addEventListener("click", saveTask);
 });
 
-
-// saving event to each time in local storage 
+// added local storage for each event 
 var hour = "";
 var task = "";
-var saveasks = {
+var saveTasks = {
   hour: hour,
   task: task,
 };
@@ -57,31 +56,10 @@ function saveTask(event) {
 
   var taskHour = $(event.target).parent().parent().find("#hour-9").val();
   console.log(taskHour);
+
 }
+
+
 //  Added code to display the current date in the header of the page.
 var currentDate = dayjs().format("dddd, MMMM D");
 $("#currentDay").text(currentDate);
-
-
-//  changing the hour to be current 
-var currentHr = dayjs().format("HH");
-var timeId = [9, 10, 12 ,13 ,14 , 15, 16, 17];
-console.log(timeId);
-console.log(typeof timeId);
-
-$(timeId).each(function(i, hour){
-  if(hour < currentHr){
-    $("#" + hour).addClass("past//")
-
-  }
-  
-}
-else if(hour == currentHr){
-  $("#" + hour).addClass("present")
-}
-
-else if(hour > currentHr){
-  $("#" + hour).addClass("#future")
-}
-
-)
